8aafc218fea8eee5be3a8bb8b30f60ae91196b51
Evan Wildenhain <evan.wildenhain@gmail.com>
2023-12-24 10:33:55 -0800

    day 18 (new approach still not quite right)

--- src/day18.rs
+++ src/day18.rs
@@ -1,31 +1,43 @@
-use std::collections::HashSet;
+use std::{collections::BTreeMap, ops::Range};
 
 use aoc_runner_derive::{aoc, aoc_generator};
+use intervaltree::IntervalTree;
 use itertools::Itertools;
-use petgraph::unionfind::UnionFind;
+use num_derive::ToPrimitive;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, ToPrimitive)]
 enum Dir {
-    Up,
-    Right,
+    Right = 0,
     Down,
     Left,
+    Up,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Dig {
     dir: Dir,
-    dist: i32,
+    dist: i64,
     color: String,
 }
 
+impl Dig {
+    fn execute(&self, loc: (i64, i64)) -> (i64, i64) {
+        let Self {
+            dir,
+            dist,
+            color: _,
+        } = self;
+        dir.apply(loc, *dist)
+    }
+}
+
 impl Dir {
-    fn apply(&self, (row, col): (i32, i32)) -> (i32, i32) {
+    fn apply(&self, (row, col): (i64, i64), n: i64) -> (i64, i64) {
         match self {
-            Dir::Up => (row - 1, col),
-            Dir::Right => (row, col + 1),
-            Dir::Down => (row + 1, col),
-            Dir::Left => (row, col - 1),
+            Dir::Up => (row - n, col),
+            Dir::Right => (row, col + n),
+            Dir::Down => (row + n, col),
+            Dir::Left => (row, col - n),
         }
     }
 }
@@ -44,7 +56,7 @@ fn parse(input: &str) -> Vec<Dig> {
                 _ => panic!("{dir}"),
             };
             let (dist, color) = rest.split_once(" ").unwrap();
-            let dist = dist.parse::<i32>().unwrap();
+            let dist = dist.parse::<i64>().unwrap();
             let color = color.strip_prefix("(#").unwrap().strip_suffix(')').unwrap();
             Dig {
                 dir,
@@ -55,65 +67,214 @@ fn parse(input: &str) -> Vec<Dig> {
         .collect_vec()
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct ColSpec {
+    col: i64,
+    hits_start: bool,
+    hits_middle: bool,
+    hits_end: bool,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum InsideState {
+    Outside,
+    Inside,
+    EnteredTop,
+    ExitedTop,
+}
+
 #[aoc(day18, part1)]
-fn part1(input: &[Dig]) -> usize {
-    let mut curr = (0i32, 0i32);
-    let mut dug = HashSet::from([curr]);
-    for Dig {
-        dir,
-        dist,
-        color: _,
-    } in input
-    {
-        for _ in 0..*dist {
-            curr = dir.apply(curr);
-            dug.insert(curr);
+fn part1(input: &[Dig]) -> i64 {
+    let digs = {
+        let mut loc = (0i64, 0i64);
+        let mut digs = Vec::new();
+        for dig in input {
+            let next = dig.execute(loc);
+            digs.push((loc, next));
+            loc = next;
         }
-    }
-
-    let (min_row, max_row) = dug.iter().map(|x| x.0).minmax().into_option().unwrap();
-    let (min_col, max_col) = dug.iter().map(|x| x.1).minmax().into_option().unwrap();
-
-    let num_spots_considered =
-        usize::try_from(((max_row + 2) - (min_row - 2) + 1) * ((max_col + 2) - (min_col - 2) + 1))
-            .unwrap();
-
-    let to_key = |(r, c)| {
-        let r = r - (min_row - 2);
-        let c = c - (min_col - 2);
-        usize::try_from(r * ((max_col + 2) - (min_col - 2) + 1) + c).unwrap()
+        digs
     };
 
-    // Using usize is a dumb hack here, petgraph should really support u64 as an IndexType.
-    let mut finder = UnionFind::<usize>::new(num_spots_considered * 2);
-
-    for r in min_row - 1..=max_row + 1 {
-        for c in min_col - 1..=max_col + 1 {
-            let loc = (r, c);
-            for neighbor in (r - 1..=r + 1).cartesian_product(c - 1..=c + 1) {
-                if dug.contains(&loc) == dug.contains(&neighbor) {
-                    finder.union(to_key(loc), to_key(neighbor));
-                }
-            }
-        }
+    for (a, b) in input.iter().tuple_windows::<(_, _)>() {
+        let dirs = [a.dir, b.dir];
+        // No double backs or continues?
+        assert_eq!(
+            dirs.contains(&Dir::Up) || dirs.contains(&Dir::Down),
+            dirs.contains(&Dir::Right) || dirs.contains(&Dir::Left)
+        );
+        // No length-1 digs?
+        assert!(a.dist > 1);
+        assert!(b.dist > 1);
     }
 
-    let outside_label = finder.find(to_key((min_row - 1, min_col - 1)));
-    finder
-        .into_labeling()
-        .into_iter()
-        .filter(|label| *label != outside_label)
-        .counts()
-        .into_iter()
-        .sorted_by_key(|x| x.1)
-        .rev()
-        .take(2)
-        .map(|x| x.1)
-        .sum()
+    let gather_rows = |digs: &[((i64, i64), (i64, i64))]| {
+        let row_to_digs_in_that_row = digs
+            .iter()
+            .filter_map(|dig| {
+                let (a, b) = *dig;
+                (a.0 == b.0).then_some((a.0, a.1.min(b.1)..a.1.max(b.1) + 1))
+            })
+            .into_grouping_map_by(|entry| {
+                let (row, _col_range) = entry;
+                *row
+            })
+            .collect::<Vec<(i64, Range<i64>)>>();
+        row_to_digs_in_that_row
+            .into_iter()
+            .map(|(k, v)| {
+                (
+                    k,
+                    IntervalTree::from_iter(v.into_iter().map(|(r, range)| {
+                        assert_eq!(k, r);
+                        (range, ())
+                    })),
+                )
+            })
+            .collect::<BTreeMap<_, _>>()
+    };
+
+    let row_to_digs_in_that_row = gather_rows(&digs);
+    let col_to_digs_in_that_col = gather_rows(
+        &digs
+            .iter()
+            .map(|(a, b)| ((a.1, a.0), (b.1, b.0)))
+            .collect_vec(),
+    );
+
+    let mut area = 0i64;
+    for (position, (rowstart, rowend)) in row_to_digs_in_that_row
+        .keys()
+        .copied()
+        .tuple_windows::<(_, _)>()
+        .with_position()
+    {
+        let is_last_row = match position {
+            itertools::Position::Last => true,
+            itertools::Position::Only => unreachable!(),
+            _ => false,
+        };
+        let height = rowend - rowstart;
+        let mut inside = InsideState::Outside;
+
+        for (position, (first_col, last_col)) in col_to_digs_in_that_col
+            .iter()
+            .filter_map(|(col, col_digs)| {
+                let hits_start = col_digs.query_point(rowstart).next().is_some();
+                let hits_middle = col_digs.query_point(rowstart + 1).next().is_some();
+                let hits_end = col_digs.query_point(rowend).next().is_some();
+                (hits_start).then_some(ColSpec {
+                    col: *col,
+                    hits_start,
+                    hits_middle,
+                    hits_end,
+                })
+            })
+            .tuple_windows::<(_, _)>()
+            .with_position()
+        {
+            let is_last_col = match position {
+                itertools::Position::First | itertools::Position::Middle => false,
+                itertools::Position::Last | itertools::Position::Only => true,
+            };
+
+            inside = match (inside, first_col.hits_middle) {
+                (InsideState::Outside, true) => InsideState::Inside,
+                (InsideState::Outside, false) => InsideState::EnteredTop,
+                (InsideState::Inside, true) => InsideState::Outside,
+                (InsideState::Inside, false) => InsideState::ExitedTop,
+                (InsideState::EnteredTop, true) => InsideState::Inside,
+                (InsideState::EnteredTop, false) => InsideState::Outside,
+                (InsideState::ExitedTop, true) => InsideState::Outside,
+                (InsideState::ExitedTop, false) => InsideState::Inside,
+            };
+
+            let width = last_col.col - first_col.col;
+
+            assert!(width > 1);
+            let to_add_bottom_length = if row_to_digs_in_that_row
+                .get(&rowend)
+                .unwrap()
+                .query_point(first_col.col + 1)
+                .next()
+                .is_some()
+            {
+                // Include the bottom of this row as well.
+                width
+            } else {
+                0
+            };
+
+            match inside {
+                InsideState::Outside => {}
+                InsideState::Inside => {
+                    area += height * width + to_add_bottom_length;
+                }
+                InsideState::EnteredTop => {
+                    // area += width;
+                }
+                InsideState::ExitedTop => {
+                    area += height * width + to_add_bottom_length;
+                }
+            }
+
+            // if matches!(inside, InsideState::Outside) && first_col.hits_end {
+            //     area += height;
+            // }
+
+            // match (inside_top, inside_bottom) {
+            //     (true, true) => {
+            //         area += height * width;
+            //     }
+            //     (true, false) => {
+            //         area += width;
+            //     }
+            //     (false, true) => {
+            //         area += width;
+            //     }
+            //     (false, false) => {
+            //         // Not inside.
+            //         // if inside_
+            //     }
+            // }
+        }
+
+        // assert!(
+        //     matches!(
+        //         inside,
+        //         InsideState::Inside | InsideState::ExitedTop | InsideState::EnteredTop
+        //     ),
+        //     "not inside at end of {rowstart}..={rowend}"
+        // );
+        // area += height;
+        // assert!(inside_top && inside_bottom);
+        // assert!(saw_anything);
+        // if is_last_row {
+        //     for dig in row_to_digs_in_that_row.get(&rowend).unwrap().iter_sorted() {
+        //         let Range { start, end } = dig.range;
+        //         area += end - start + 1;
+        //     }
+        // }
+    }
+
+    // for elem in row_to_digs_in_that_row
+    //     .first_key_value()
+    //     .unwrap()
+    //     .1
+    //     .iter_sorted()
+    // {
+    //     let Range { start, end } = elem.range;
+    //     area += end - start;
+    // }
+
+    // for dig in input {
+    //     area += dig.dist;
+    // }
+    area
 }
 
 #[aoc(day18, part2)]
-fn part2(input: &[Dig]) -> usize {
+fn part2(input: &[Dig]) -> i64 {
     let input = input
         .into_iter()
         .map(
@@ -123,7 +284,7 @@ fn part2(input: &[Dig]) -> usize {
                  color,
              }| {
                 let dist = &color[..5];
-                let dist = i32::from_str_radix(dist, 16).unwrap();
+                let dist = i64::from_str_radix(dist, 16).unwrap();
 
                 let dir = &color[5..];
                 let dir = match dir {
